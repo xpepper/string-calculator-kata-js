b6dce56264e57398c6279e3c92193c5db94c8e7e
Pietro Di Bello <pierodibello@betelgeuse.local>
2016-12-11 23:33:04 +0100

    remove all code so that this is a good starting point for the kata

--- test/string-calculator-test.js
+++ test/string-calculator-test.js
@@ -1,22 +1,6 @@
-const assert = require('assert')
-const calculator = require('../lib/string-calculator')
 
 describe('string-calculator', function () {
   describe('add', function () {
-    it('is 0 for an empty string', function () {
-      assert.equal(calculator.add(''), 0)
-    })
-
-    it('is a number for a string with only a number', function () {
-      assert.equal(calculator.add('1'), 1)
-    })
-
-    it('is the sum of the numbers for a string of any amount of comma-separated numbers', function () {
-      assert.equal(calculator.add('1,2,3'), 6)
-    })
-
-    it('handles also \\n as number delimiter', function () {
-      assert.equal(calculator.add('1\n2'), 3)
-    })
+    it('is your first test', function () {})
   })
 })

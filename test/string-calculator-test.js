b44ad12ac598d97198a54c8a1c5efd9a520eaddc
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-12-12 18:07:52 +0100

    added customDelimiterParser

--- test/string-calculator-test.js
+++ test/string-calculator-test.js
@@ -2,23 +2,44 @@ const {equal} = require('assert')
 
 describe('StringCalculator', function () {
   const StringCalculator = require('../lib/string-calculator')
-  it('returns 0 on empty string', function () {
-    equal(StringCalculator.add(''), 0)
+
+  describe('#add', function () {
+    const {add} = StringCalculator
+
+    it('returns 0 on empty string', function () {
+      equal(add(''), 0)
+    })
+
+    it('returns number for a single number string', function () {
+      equal(add('1'), 1)
+    })
+
+    it('returns sum for a comma separated string with two values', function () {
+      equal(add('1,2'), 3)
+    })
+
+    it('returns sum for an unknown number of comma separated values', function () {
+      equal(add('1,2,3,4'), 10)
+    })
+
+    it('returns sum for an unknown number of an end-of-line separated values', function () {
+      equal(add('1\n2'), 3)
+    })
+
+    it.skip('returns sum for custom delimited values', function () {
+      equal(add('//x\n1x2'), 3)
+    })
   })
 
-  it('returns number for a single number string', function () {
-    equal(StringCalculator.add('1'), 1)
-  })
+  describe('#parseCustomDelimiter', function () {
+    const {parseCustomDelimiter: parse} = StringCalculator
 
-  it('returns sum for a comma separated string with two values', function () {
-    equal(StringCalculator.add('1,2'), 3)
-  })
+    it('returns empty string if no custom delimiter is specified', function () {
+      equal(parse('1,2'), '')
+    })
 
-  it('returns sum for an unknown number of comma separated values', function () {
-    equal(StringCalculator.add('1,2,3,4'), 10)
-  })
-
-  it('returns sum for an unknown number of an end-of-line separated values', function () {
-    equal(StringCalculator.add('1\n2'), 3)
+    it('returns a custom delimiter', function () {
+      equal(parse('//x\n1x2'), 'x')
+    })
   })
 })

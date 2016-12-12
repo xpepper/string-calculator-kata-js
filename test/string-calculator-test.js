77c2c4bf267100fbd77dbb7f1e390ede67a253f1
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-12-12 17:10:02 +0100

    handles empty string

--- test/string-calculator-test.js
+++ test/string-calculator-test.js
@@ -1,22 +1,8 @@
-const assert = require('assert')
-const calculator = require('../lib/string-calculator')
+const {equal} = require('assert')
 
-describe('string-calculator', function () {
-  describe('add', function () {
-    it('is 0 for an empty string', function () {
-      assert.equal(calculator.add(''), 0)
-    })
-
-    it('is a number for a string with only a number', function () {
-      assert.equal(calculator.add('1'), 1)
-    })
-
-    it('is the sum of the numbers for a string of any amount of comma-separated numbers', function () {
-      assert.equal(calculator.add('1,2,3'), 6)
-    })
-
-    it('handles also \\n as number delimiter', function () {
-      assert.equal(calculator.add('1\n2'), 3)
-    })
+describe('StringCalculator', function () {
+  const StringCalculator = require('../lib/string-calculator')
+  it('returns 0 on empty string', function () {
+    equal(StringCalculator.add(''), 0)
   })
 })
